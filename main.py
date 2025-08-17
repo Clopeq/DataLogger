b8958b186ee7b3b49ee1a6521cd6497ab45632aa
Seba <259813@student.pwr.edu.pl>
2025-08-17 15:32:21 +0200

    rearanged the threads

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import eel
 import os
 import threading
 
-DEV_MODE = True
+DEV_MODE = False
 
 eel.init('app')
 
@@ -14,25 +14,23 @@ if DEV_MODE:
 else:
     mode="netsurf"
 
-print("Starting the browser...")
-os.popen('netsurf http://DAQ:8080')
+#print("Starting the browser...")
+#os.popen('netsurf http://DAQ:8080')
 
-print("Starting the server...")
-eel.start('index.html', size=(1024,600), mode=mode, host="0.0.0.0", port=8080, block=False)
-
-#ADC1 = ADC.ADS1263('GAIN_1', '14400SPS')
-
-#def read_adc_loop():
-#    while True:
-#        print("\033[H\033[J", end="")  # console clear
-#        print("A0: %f" % ADC1.read(0))
-#        time.sleep(0.01)
+def web_server():
+    print("Starting the application...")
+    eel.start('index.html', size=(1024,600), mode=mode, host="0.0.0.0", port=8000)
 
 # Run ADC loop in a separate thread
-#adc_thread = threading.Thread(target=read_adc_loop)
-#adc_thread.daemon = True
-#adc_thread.start()
+app_thread = threading.Thread(target=web_server)
+app_thread.daemon = True
+app_thread.start()
 
-# Keep the main thread alive so Eel stays running
+
+# main loop
+ADC1 = ADC.ADS1263('GAIN_1', '14400SPS')
+
 while True:
-    time.sleep(1)
+    print("\033[H\033[J", end="")  # console clear
+    print("A0: %f" % ADC1.read(0))
+    time.sleep(0.01)
