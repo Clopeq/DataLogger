b1276f75ebe7cb946933b7d2a2fd03bec59cfa9d
Seba <259813@student.pwr.edu.pl>
2025-08-17 12:32:27 +0200

    Raw data readout sample

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("World, Hellp!")
+import backend.ADS1263 as ADC
+
+
+REF = 5.03
+
+ADC1 = ADC.ADS1263('GAIN_1', '14400SPS')
+
+while True:
+    print("A0: %f" % ADC1.GetChannalValue(0))
+    print("A1: %f" % ADC1.GetChannalValue(1))
+    print("A2: %f" % ADC1.GetChannalValue(2))
+    print("A3: %f" % ADC1.GetChannalValue(3))
+    print("A4: %f" % ADC1.GetChannalValue(4))
+    print("A5: %f" % ADC1.GetChannalValue(5))
+    print("A6: %f" % ADC1.GetChannalValue(6))
+    print("A7: %f" % ADC1.GetChannalValue(7))
+
