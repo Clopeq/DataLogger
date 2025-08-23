39fc0a904c0059bfca2739c07e6855422a42782b
Seba <259813@student.pwr.edu.pl>
2025-08-23 17:00:14 +0200

    ADS1263 testing3

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from backend import ADS1263
 from time import time
 
-ADC = ADS1263.ADS1263('GAIN_!', '14400SPS', ref=5.03)
+ADC = ADS1263.ADS1263('GAIN_1', '14400SPS', ref=5.03)
+
+channels = [0,1]
+string = ""
 
 t = time()
 while time()-t < 5:
-    print("A0: ", ADC.read(0))
+    for ch in channels:
+        string += f"A{ch}: {ADC.read(ch)} | "
+    print(string[:-2])
+    print("\n")
